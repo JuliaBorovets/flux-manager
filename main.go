8cef228b47462ebfa42ca56fe262843f1e065551
julia <juliaborovets2001@gmail.com>
2023-10-12 11:54:57 +0300

    NO JIRA

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"log"
 )
@@ -8,6 +9,7 @@ import (
 func main() {
 	app := fiber.New()
 
+	app.Get("/test", testHandler)
 	app.Get("/healthy", healthyHandler)
 	app.Get("/ready", readyHandler)
 
@@ -19,5 +21,12 @@ func healthyHandler(ctx *fiber.Ctx) error {
 }
 
 func readyHandler(ctx *fiber.Ctx) error {
+	return ctx.SendStatus(200)
+}
+
+func testHandler(ctx *fiber.Ctx) error {
+	agent := fiber.Get("http://notification-receiver.notifications-test/ready")
+	s, r, e := agent.Bytes()
+	fmt.Printf("---%v, ----%v, ----%e\n", s, r, e)
 	return ctx.SendStatus(200)
 }
